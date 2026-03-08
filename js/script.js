bfe69fb3d009f0fd35436c9061a47744209bdbb9
annalena-project <aschaeffer@northpark.edu>
2026-03-07 19:47:53 -0600

    Added, select gif container and button from HTML

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 console.log("script.js loaded");
 
+const gifContainer = document.querySelector("#gif-container");
+const button = document.querySelector("fetch-gif");
+
 const endpoint =
 "https://api.giphy.com/v1/gifs/search?api_key=LLenvCeIrVVm10joOahJcoiP1VgXJiMj&q=dogs&limit=25&offset=0&rating=g&lang=en";
 
