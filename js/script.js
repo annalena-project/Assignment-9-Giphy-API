533afaead9207c514acd7c1263ecd2f599b84674
annalena-project <aschaeffer@northpark.edu>
2026-03-07 19:12:47 -0600

    Added Giphy API fetch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("script.js loaded");
+
+const endpoint =
+"https://api.giphy.com/v1/gifs/search?api_key=LLenvCeIrVVm10joOahJcoiP1VgXJiMj&q=dogs&limit=25&offset=0&rating=g&lang=en";
+
+fetch(endpoint)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  });
