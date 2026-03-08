dcac4961297ed74ac1c1aab2ca1cf29852f7a969
annalena-project <aschaeffer@northpark.edu>
2026-03-08 17:52:29 -0500

    Fix button , display gifs on page and debug

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 console.log("script.js loaded");
 
 const gifContainer = document.querySelector("#gif-container");
-const button = document.querySelector("fetch-gif");
+const button = document.querySelector("#fetch-gif-btn");
 
 const endpoint =
 "https://api.giphy.com/v1/gifs/search?api_key=LLenvCeIrVVm10joOahJcoiP1VgXJiMj&q=dogs&limit=25&offset=0&rating=g&lang=en";
@@ -11,4 +11,18 @@ fetch(endpoint)
   .then(data => {
     const images = data.data;
     console.log(images);
+
+    button.addEventListener("click", function () {
+
+      gifContainer.innerHTML = "";
+
+      for (let image of images) {
+        const url = image.images.original.url;
+
+        gifContainer.innerHTML +=
+          `<img src="${url}" class="col-3 mb-3">`;
+      }
+
+    });
+
   });
